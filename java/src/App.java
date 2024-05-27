f7315afd8793699a3a85ec18ea4615cb9157510a
Sebastian Feldmann <sf@sebastian-feldmann.info>
2024-05-27 15:00:05 +0200

    Add java version

---
+++ java/src/App.java
@@ -0,0 +1,8 @@
+public class App
+{
+    public static void main(String[] args)
+    {
+        House house = new House();
+        System.out.println(house.recite());
+    }
+}
---
+++ java/src/House.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class House
+{
+    protected String[] parts = {
+        "the house that jack built",
+        "the malt that lay in",
+        "the rat that ate",
+        "the cat that killed",
+        "the dog that worried",
+        "the cow with the crumpled horn that tossed",
+        "the maiden all forlorn that milked",
+        "the man all tattered and torn that kissed",
+        "the priest all shaven and shorn that married",
+        "the rooster that crowed in the morn that woke",
+        "the farmer sowing his corn that kept",
+        "the horse and the hound and the horn that belonged to",
+    };
+
+    public String recite()
+    {
+       return "This is...";
+    }
+}
